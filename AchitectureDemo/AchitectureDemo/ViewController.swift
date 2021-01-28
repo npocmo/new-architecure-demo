5b554072afdef2679ae27a90e0cc071be9ead94a
Dimitri Nesterenko <dimitri.nesterenko@comdirect.de>
2021-01-28 09:25:14 +0100

    initial project commit

---
+++ AchitectureDemo/AchitectureDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AchitectureDemo
+//
+//  Created by Dimitri Nesterenko on 28.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
